82b28af085c372392ac53e7b8206be1fc513b6aa
Brad Ringel <brad.ringel@gmail.com>
2015-04-28 19:46:12 -0500

    Initial commit from Xcode

---
+++ Superminder/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Superminder
+//
+//  Created by Bradley Ringel on 4/28/15.
+//  Copyright (c) 2015 Bradley Ringel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
