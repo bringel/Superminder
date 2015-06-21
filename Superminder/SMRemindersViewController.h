e8758abed6732f08c66c9041a2efc8c965725feb
Brad Ringel <brad.ringel@gmail.com>
2015-06-21 16:40:49 -0400

    Swap UICollectionView for UITableView in main VC

--- Superminder/SMRemindersViewController.h
+++ Superminder/SMRemindersViewController.h
@@ -8,6 +8,6 @@
 
 #import <UIKit/UIKit.h>
 
-@interface SMRemindersViewController : UICollectionViewController
+@interface SMRemindersViewController : UITableViewController
 
 @end
--- Superminder/SMRemindersViewController.m
+++ Superminder/SMRemindersViewController.m
@@ -18,7 +18,7 @@
 
 @implementation SMRemindersViewController
 
-static NSString * const reuseIdentifier = @"cardCollectionViewCell";
+static NSString * const reuseIdentifier = @"Cell";
 
 - (void)viewDidLoad {
     [super viewDidLoad];
@@ -27,7 +27,6 @@ static NSString * const reuseIdentifier = @"cardCollectionViewCell";
     // self.clearsSelectionOnViewWillAppear = NO;
     
     // Register cell classes
-    [self.collectionView registerClass:[UICollectionViewCell class] forCellWithReuseIdentifier:reuseIdentifier];
     
     [[NSNotificationCenter defaultCenter] addObserverForName:kNeedsReauthentication object:nil queue:[NSOperationQueue mainQueue] usingBlock:^(NSNotification *note) {
         [self performSegueWithIdentifier:@"showLoginScreen" sender:self];
@@ -36,11 +35,6 @@ static NSString * const reuseIdentifier = @"cardCollectionViewCell";
     self.trelloKey = [Lockbox stringForKey:kTrelloUserKey];
     if(!self.trelloKey){
         [self performSegueWithIdentifier:@"showLoginScreen" sender:self];
-    }
-    else{
-        SMTrelloClient *client = [SMTrelloClient sharedClient];
-        [client getCurrentUserInfo];
-        [client getAlBoardDataForUser: client.currentUser];
     }
 }
 
@@ -58,57 +52,28 @@ static NSString * const reuseIdentifier = @"cardCollectionViewCell";
     // Pass the selected object to the new view controller.
 }
 
+#pragma mark - <UITableViewDataSource>
 
-#pragma mark <UICollectionViewDataSource>
-
-- (NSInteger)numberOfSectionsInCollectionView:(UICollectionView *)collectionView {
-#warning Incomplete method implementation -- Return the number of sections
-    return 0;
-}
-
-
-- (NSInteger)collectionView:(UICollectionView *)collectionView numberOfItemsInSection:(NSInteger)section {
-#warning Incomplete method implementation -- Return the number of items in the section
-    return 0;
-}
-
-- (UICollectionViewCell *)collectionView:(UICollectionView *)collectionView cellForItemAtIndexPath:(NSIndexPath *)indexPath {
-    UICollectionViewCell *cell = [collectionView dequeueReusableCellWithReuseIdentifier:reuseIdentifier forIndexPath:indexPath];
+- (NSInteger) numberOfSectionsInTableView:(UITableView *)tableView{
     
-    // Configure the cell
+    return 1;
+}
+
+//- (NSArray *) sectionIndexTitlesForTableView:(UITableView *)tableView{
+//    
+//}
+
+- (NSInteger) tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section{
+    
+    return 1;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath{
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:reuseIdentifier forIndexPath:indexPath];
+    
     
     return cell;
 }
-
-#pragma mark <UICollectionViewDelegate>
-
-/*
-// Uncomment this method to specify if the specified item should be highlighted during tracking
-- (BOOL)collectionView:(UICollectionView *)collectionView shouldHighlightItemAtIndexPath:(NSIndexPath *)indexPath {
-	return YES;
-}
-*/
-
-/*
-// Uncomment this method to specify if the specified item should be selected
-- (BOOL)collectionView:(UICollectionView *)collectionView shouldSelectItemAtIndexPath:(NSIndexPath *)indexPath {
-    return YES;
-}
-*/
-
-/*
-// Uncomment these methods to specify if an action menu should be displayed for the specified item, and react to actions performed on the item
-- (BOOL)collectionView:(UICollectionView *)collectionView shouldShowMenuForItemAtIndexPath:(NSIndexPath *)indexPath {
-	return NO;
-}
-
-- (BOOL)collectionView:(UICollectionView *)collectionView canPerformAction:(SEL)action forItemAtIndexPath:(NSIndexPath *)indexPath withSender:(id)sender {
-	return NO;
-}
-
-- (void)collectionView:(UICollectionView *)collectionView performAction:(SEL)action forItemAtIndexPath:(NSIndexPath *)indexPath withSender:(id)sender {
-	
-}
-*/
+#pragma mark - <UITableViewDelegate>
 
 @end
