92035abb8a0772ee3db223c1fd7dd5fdc7266b60
Brad Ringel <brad.ringel@gmail.com>
2015-07-02 20:11:30 -0500

    Add new reminder view controller

---
+++ Superminder/SMNewReminderViewController.h
@@ -0,0 +1,13 @@
+//
+//  SMNewReminderViewController.h
+//  Superminder
+//
+//  Created by Bradley Ringel on 7/2/15.
+//  Copyright (c) 2015 Bradley Ringel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SMNewReminderViewController : UITableViewController
+
+@end
